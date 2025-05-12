23637980c7daf3a22dc2a62318c84ad0d7bd00a1
Utkarsh <catci142@gmail.com>
2025-05-12 12:06:27 +0530

    pull

--- server/routes/licenseRoutes.js
+++ server/routes/licenseRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const licenseController = require('../controllers/licenseController');
-const uploadMiddleware = require('../middleware/uploadMiddleware');
+const upload = require('../middleware/uploadMiddleware');
 const { verifyToken } = require("../middleware/authMiddleware");
 
 // Make sure these are properly imported functions
-router.post('/upload', uploadMiddleware, licenseController.uploadVaccinationProof);
+router.post('/upload', upload, licenseController.uploadVaccinationProof);
 router.post('/apply', verifyToken, licenseController.applyLicense);
 router.get('/user', verifyToken, licenseController.getUserLicenses);
 router.delete('/:id', licenseController.deleteLicense);
